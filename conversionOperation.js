c9b465505e6416b1381ac151c7f135c375ded44b
Karan8826 <rajputvkaran0401@gmail.com>
2025-08-15 14:31:45 +0530

    conversionoperation

---
+++ conversionOperation.js
@@ -0,0 +1,29 @@
+let score = null
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof  valueInNumber);
+// console.log(valueInNumber);
+
+// when you convert a number like "33" => then its easly change in 33
+// when no is "33abc" => NaN
+// true => 1; false=> 0
+
+let isLoggedIn = "karan"
+
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+//   console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+//  "karan" => true
+
+let someNumber = 33
+
+let stringNumber = string(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
